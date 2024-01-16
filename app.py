5ff96f3f236734b9c7c066e867c43855f43137ef
Surya Govindasamy Ganesan <suryaganesan925@gmail.com>
2024-01-16 19:51:46 +0000

    kbk

--- app.py
+++ app.py
@@ -1,35 +1,37 @@
 import firebase_admin
 from firebase_admin import credentials
-from flask import Flask
-from pyngrok import ngrok
-from flask_ngrok import run_with_ngrok
+from nordigen.nordigen import NordigenClient
+from uuid import uuid4
+import json
+from flask import Flask, jsonify
 
 cred = credentials.Certificate("/Users/suryaganesan/vscode/finance/guilty_re/serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
-#initializing flask server
-ngrok.set_auth_token("ak_2azm3M9tC6lbHmGDtmLlpNUh0vP")
+client = NordigenClient(
+    secret_id="b5314ee2-e221-4a84-96c0-ec97d252896c",
+    secret_key="82380ff2da7d792baaa244062fb5538401397f1ec27f1e83c1b35da8e15c1ffb0e130d171f1c585e486d4ae72fb93abd049c480372aeed5daf48630bbd6e5e16"
+)
 
-app = Flask(__name__) 
-#run_with_ngrok(app) 
-port = 5000
+app = Flask(__name__)
+
+def get_banklist():
+    token_data = client.generate_token()
+    access_token = token_data["access"]
+
+    institutions_dict = client.institution.get_institutions("GB")
+    
+    institutions = [d.get("name") for d in institutions_dict]
+
+    return jsonify(institutions)
+
+@app.route('/onboarding/get_banklist')
+def get_banklist_route():
+   inst = get_banklist()
+
+   return inst, 200
 
 
-#Onboarding
+#get_banklist()
 
-# 1) Send institution list for dropdown
-def banklist():
-    pass
-
-@app.route('/onboarding/banklist')
-def banklist_route():
-    return "None"
-
-# 2) Take institution name, find inst. id, return requisition id and authorization link
-
-#Homepage
-
-# 1) Take in pay date, budget amount, requisition id and return day spend, budget health, budget left nad days to pay date
-
-
-app.run()
+#app.run(debug=True)
